836223d12a946dbb7b2e489cbc6b12a601a7bb58
Erick Ribeiro Araujo <ereck@live.com>
2023-11-06 21:15:34 -0300

    exercicio 14

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,9 @@
+function Contar() {
+    var inicio = Number(document.querySelector('#inicio'))
+    var fim = Number(document.querySelector('#fim'))
+    var passo = Number(document.querySelector('#passo'))
+
+    for (i = inicio; i <= fim; i =+ passo){
+        
+    }
+}
