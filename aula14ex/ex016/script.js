91eaf5c59c27ef0510222bf420b422114b8330f5
Erick Ribeiro Araujo <ereck@live.com>
2023-11-08 17:28:59 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,6 +4,35 @@ function Contar() {
     var passo = Number(document.querySelector('#passo').value)
     var res = document.querySelector('div#res')
 
+    if (inicio == '' || inicio >= fim){
+        res.innerHTML = 'Impossivel contar!'
+        if (passo == 0){
+            passo = 1
+        }
+    
+    } else {
+        
+        for (i = inicio; i <= fim; i += passo){
+            if (i == inicio) {
+                res.innerHTML = `${i}`
+                res.innerHTML += `👉`
+            } else if (i < fim){
+                res.innerHTML += `${i}`
+                res.innerHTML += `👉`
+            } else if (i == fim){
+                res.innerHTML += `${i}`
+            } else {
+                res.innerHTML += `${i}`
+            }
+            
+        }
+
+        res.innerHTML += `🏴`
+
+
+    }
+
+    /*
     for (i = inicio; i <= fim; i += passo){
         res.innerHTML += `${i}`
         
@@ -13,4 +42,6 @@ function Contar() {
             res.innerHTML += `🏴`
         }
     }
+    */
 }
+
