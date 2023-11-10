422b85fa2c067da965db808e80376a94d7fd165a
Erick Ribeiro Araujo <ereck@live.com>
2023-11-10 16:23:57 -0300

    Exercício da tabuado concluído!
    Nossa, essa foi curiosamente facil hahaha

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,25 @@
-alert('Olá!')
+function gerarTabuada(){
+    
+    let numero = document.querySelector('#txtnumero')
+    let campo = document.querySelector('#ires')
+
+    if (numero.value.length == 0){
+        window.alert('[ERRO]Preencha o campo numero!')
+    } else {
+        
+        campo.innerHTML = ''
+        
+        for (i=1;i<=10; i++){
+            let opcao = document.createElement('option')
+            var n = Number(numero.value)
+            var res = n * i
+            opcao.setAttribute ('value',`${n}x${i}=${res}`)
+            opcao.innerHTML = `${n}X${i}=${res}` 
+            campo.appendChild(opcao)
+        }
+
+    }
+
+}
+
+
