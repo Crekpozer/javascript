654b2e458a503ac27b9e5ad174c349ec4dbac8a2
Erick Ribeiro Araujo <ereck@live.com>
2023-11-16 10:29:24 -0300

    Commit do inicio do dia
    Salvando alterações não salva do ultimo dia de trabalho

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [4, 8, 2, 5, 3]
+num.push(1)
+num.sort()
+
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+console.log()
